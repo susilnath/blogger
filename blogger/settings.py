c631809e64b72b513b98b066d7bc9e7b3e2da7da
susilnath <57358192+susilnath@users.noreply.github.com>
2020-07-12 15:46:43 +0530

    Update settings.py

--- blogger/settings.py
+++ blogger/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
+#import dj_database_url
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
