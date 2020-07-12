c6ea8f9df3970c8186243294b3012590b85a7835
susilnath <57358192+susilnath@users.noreply.github.com>
2020-07-12 14:38:10 +0530

    DatabaseChanged
    using postgresql

--- blogger/settings.py
+++ blogger/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,6 +76,8 @@ WSGI_APPLICATION = 'blogger.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 DATABASES = {
     'default': {
