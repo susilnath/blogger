be8314833df1b3041891e63a1eaae3ae76962217
susilnath <57358192+susilnath@users.noreply.github.com>
2020-07-12 15:26:18 +0530

    Update settings.py

--- blogger/settings.py
+++ blogger/settings.py
@@ -77,15 +77,15 @@ WSGI_APPLICATION = 'blogger.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = { 'default': dj_database_url.config() }
+#db_from_env = dj_database_url.config(conn_max_age=500)
+#DATABASES = { 'default': dj_database_url.config() }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
